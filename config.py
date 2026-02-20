6edfed251a312098807e5d5b0ec52acb9725cc9d
Git2683 <bake25bake@gmail.com>
2026-02-20 21:51:09 +0500

    config.py

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("8428844583:AAHoflkJsD8nhJM597h2irp9_ZvRQzf67oY")
+BOT_TOKEN = os.getenv("8361410975:AAE3lEQX03HgzQ6leoGCd4AqSmulEIaTOa8")
 OPENAI_API_KEY = os.getenv("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")
 
 # Для Telegram Stars оставьте пустым
-PAYMENTS_PROVIDER_TOKEN = os.getenv("8361410975:AAE3lEQX03HgzQ6leoGCd4AqSmulEIaTOa8")
+PAYMENTS_PROVIDER_TOKEN = os.getenv("6395449203:TEST:177336b1fa59818ad197")
